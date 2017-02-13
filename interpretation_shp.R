f0cea0da1073a9c4a34a3633853d90f3dbe0c16a
Marike <marikemeijer@uni-muenster.de>
2017-02-13 14:21:35 +0100

    add r script shapefile

--- interpretation_shp.R
+++ interpretation_shp.R
@@ -1,19 +1,46 @@
 ##shapefile-results
 
 shp=read.table("ticks_2Dshape_results.csv", sep ="",header=TRUE)
-shp_uniform=shp[which(shp$distribution=="uniform"),]
+shp_uniform=shp[which(shp$distribution=="uniform"&shp$asd==0.0),]
+shp_uniform_05=shp[which(shp$distribution=="uniform"&shp$asd==0.5),]
 shp_random=shp[which(shp$distribution=="random"),]
-twod=read.table("ticks_2D_results.csv", sep ="",header=TRUE)
-twod_uniform=twod[which(twod$gb.influence=="false"&twod$aq.distribution=="uniform"&twod$asd=="0"&twod$greenbelt.width=="1"),]
-twod_random=twod[which(twod$gb.influence=="false"&twod$aq.distribution=="random"&twod$greenbelt.width=="1"),]
 
 
-##t-test uniform
-t.test(twod_uniform$ticks*.1,shp_uniform$ticks*.1)
+shp_uniform_15=shp[which(shp$distribution=="uniform"&shp$available.locations==15&shp$asd==0.0),]
+shp_uniform_30=shp[which(shp$distribution=="uniform"&shp$available.locations==30&shp$asd==0.0),]
+shp_uniform_45=shp[which(shp$distribution=="uniform"&shp$available.locations==45&shp$asd==0.0),]
+shp_uniform_15_05=shp[which(shp$distribution=="uniform"&shp$available.locations==15&shp$asd==0.5),]
+shp_uniform_30_05=shp[which(shp$distribution=="uniform"&shp$available.locations==30&shp$asd==0.5),]
+shp_uniform_45_05=shp[which(shp$distribution=="uniform"&shp$available.locations==45&shp$asd==0.5),]
+shp_random_15=shp[which(shp$distribution=="random"&shp$available.locations==15),]
+shp_random_30=shp[which(shp$distribution=="random"&shp$available.locations==30),]
+shp_random_45=shp[which(shp$distribution=="random"&shp$available.locations==45),]
+
+mean(shp_uniform_15$ticks*.1)
+sd(shp_uniform_15$ticks*0.1)
+mean(shp_uniform_30$ticks*.1)
+sd(shp_uniform_30$ticks*0.1)
+mean(shp_uniform_45$ticks*.1)
+sd(shp_uniform_45$ticks*0.1)
+mean(shp_uniform_15_05$ticks*.1)
+sd(shp_uniform_15_05$ticks*0.1)
+mean(shp_uniform_30_05$ticks*.1)
+sd(shp_uniform_30_05$ticks*0.1)
+mean(shp_uniform_45_05$ticks*.1)
+sd(shp_uniform_45_05$ticks*0.1)
+mean(shp_random_15$ticks*.1)
+sd(shp_random_15$ticks*0.1)
+mean(shp_random_30$ticks*.1)
+sd(shp_random_30$ticks*0.1)
+mean(shp_random_45$ticks*.1)
+sd(shp_random_45$ticks*0.1)
+
+
+##t-test uniform 1 & 2
+##Proove the influence of service center prevent sprawl:
+t.test(shp_uniform$ticks*0.1,shp_uniform_05$ticks*0.1)
 
 ##t-test random
-t.test(twod_random$ticks*.1,shp_random$ticks*.1)
-
-hist(shp_random$ticks)
-hist(shp_uniform$ticks)
+#Proove the influence of existing astethic quality on greenbelt preventing sprawl:
+t.test(shp_uniform_05$ticks*0.1,shp_random$ticks*0.1)
 
